e3923b184a82bfbe1b502eebf477f1b13c61f00d
AmaliaSwastika <swastika.amalia@gmail.com>
2024-07-20 08:41:24 +0700

    Update Profile Screen

--- lib/screen/profile/profile_screen.dart
+++ lib/screen/profile/profile_screen.dart
@@ -3,6 +3,8 @@ import 'package:dameyu_project/theme/theme_color.dart';
 import 'package:dameyu_project/theme/theme_text_style.dart';
 import 'package:dameyu_project/utils/shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -12,60 +14,132 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+
+    String _username = "";
+  File? _profileImage;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _profileImage = File(image.path);
+      });
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+    _getUsername().then((username) {
+      if (mounted) {
+        setState(() {
+          _username = username;
+        });
+      }
+    });
+  }
+
+  Future<String> _getUsername() async {
+    final username = await SharedPref().getToken();
+    return username;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ThemeColor().whiteColor,
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(70.0),
-        child: AppBar(
-          backgroundColor: ThemeColor().pinkColor,
-          centerTitle: true,
-          title: Align(
-            alignment: Alignment.centerLeft,
-            child: Image.asset(
-              'assets/logo2.png',
-            ),
+  backgroundColor: ThemeColor().whiteColor,
+  appBar: PreferredSize(
+    preferredSize: const Size.fromHeight(70.0),
+    child: AppBar(
+      backgroundColor: ThemeColor().pinkColor,
+      centerTitle: true,
+      title: Row(
+        children: [
+          // Logo
+          Image.asset(
+            'assets/logo2.png',
+            width: 50, // Atur lebar logo sesuai kebutuhan
+            height: 50, // Atur tinggi logo sesuai kebutuhan
           ),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(40),
-              bottomRight: Radius.circular(40),
-            ),
+          // Spacer to push title to the center
+          Spacer(),
+          // Title
+          Text(
+            'Profile',
+            style: ThemeTextStyle().login, // Gaya teks untuk judul
           ),
-        ),
+          // Spacer to ensure the title is centered
+          Spacer(),
+        ],
       ),
-      body: Center(
-      child: ElevatedButton(
-        onPressed: () async {
-          Navigator.pushAndRemoveUntil(
-            context,
-            MaterialPageRoute(
-              builder: (_) => const SplashScreen(),
-            ),
-            (route) => false,
-          );
-          await SharedPref().removeToken();
-        },
-        child: Text(
-          'Logout',
-          style: ThemeTextStyle().login,
-        ),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: ThemeColor().pinkColor, // Warna latar belakang tombol
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          padding: const EdgeInsets.symmetric(horizontal: 100.0, vertical: 7.0),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(40),
+          bottomRight: Radius.circular(40),
         ),
       ),
     ),
-  );
-}
-    
+  ),
+      
+      body: Center(
+        
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _profileImage != null
+                ? CircleAvatar(
+                    radius: 50,
+                    backgroundImage: FileImage(_profileImage!),
+                  )
+                : CircleAvatar(
+                    radius: 50,
+                    backgroundColor: ThemeColor().greyColor,
+                    child: IconButton(
+                      icon: Icon(Icons.camera_alt, color: ThemeColor().whiteColor),
+                      onPressed: _pickImage,
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text(
+                'Username : $_username',
+                style: ThemeTextStyle().welcomeUsername,
+              ),
+            ),
+          ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const SplashScreen(),
+                  ),
+                  (route) => false,
+                );
+                await SharedPref().removeToken();
+              },
+              child: Text(
+                'Logout',
+                style: ThemeTextStyle().login,
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: ThemeColor().pinkColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 70.0, vertical: 7.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
